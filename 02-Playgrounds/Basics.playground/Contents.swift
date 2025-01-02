c5c0c85d14b989213c82c6901ae1514abb3585fd
Junsoo Kim <kel_777@naver.com>
2025-01-02 13:47:09 +0900

    If - let

--- 02-Playgrounds/Basics.playground/Contents.swift
+++ 02-Playgrounds/Basics.playground/Contents.swift
@@ -11,19 +11,30 @@ import Foundation
 //runPropertyObserver() // 프로토콜 버저버
 
 // 제어 흐름
-enum PoolBallType {
+enum PoolBallType: String {
     case solid
     case stripe
     case black
 }
 
-func poolBallType(forNumber number: Int) ->  PoolBallType {
-    if number < 8 {
+func poolBallType(forNumber number: Int) ->  PoolBallType? {
+    if number < 8 && number > 0 {
         return .solid
-    } else if number > 8 {
+    } else if number > 8 && number < 16 {
         return .stripe
-    } else {
+    } else if number == 8 {
         return .black
+    } else {
+        return nil
+    }
+}
+
+func printBallDetails(ofNumber number: Int) {
+    let possibleBallType = poolBallType(forNumber: number)
+    if let ballType = possibleBallType {
+        print("\(number) - \(ballType.rawValue)")
+    } else {
+        print("\(number) is not a valid pool ball number")
     }
 }
 
@@ -31,4 +42,11 @@ let two = poolBallType(forNumber: 2)
 let eight = poolBallType(forNumber: 8)
 let twoelve = poolBallType(forNumber: 12)
 
-print(two)
+let zero = poolBallType(forNumber: 0)
+let sixteen = poolBallType(forNumber: 16)
+
+printBallDetails(ofNumber: 2)
+printBallDetails(ofNumber: 8)
+printBallDetails(ofNumber: 12)
+printBallDetails(ofNumber: 0)
+printBallDetails(ofNumber: 16)
