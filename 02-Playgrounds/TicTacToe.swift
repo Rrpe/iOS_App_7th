2c29637c7ab3fed079a4f2f4aa4d89494fa2f5a3
Junsoo Kim <kel_777@naver.com>
2025-01-03 13:31:44 +0900

    isValidMove, makeMove 함수 추가

--- 02-Playgrounds/TicTacToe.swift
+++ 02-Playgrounds/TicTacToe.swift
@@ -28,22 +28,34 @@ class TicTacToe: GameState {
     // 유효한 움직임인지 확인
     func isValidMove(row: Int, column: Int) -> Bool {
         // TODO: isValidMove 메서드 구현
-        return false
+        let isValid = board[row][column] == .empty
+        return isValid
     }
     
     func makeMove(row: Int, column: Int) -> Bool {
         // TODO: 플레이어 이동 메서드 구현
+        guard isValidMove(row: row, column: column) else {
+            return false
+        }
+        board[row][column] = currentPlayer
         
-        return false
+        // 플레이어 차례 변경
+        if currentPlayer == .x {
+            currentPlayer = .o
+        } else {
+            currentPlayer = .x
+        }
+        
+        return true
     }
     
     func checkWinner() -> Piece? {
-        // 승리조건
+        // 승리조건 Add_2
         // 가로 [0,0] [0,1] [0,2], [1,0] [1,1] [1,2], [2,0] [2,1] [2,2]
         // 세로 [0,0] [1,0] [2,0], [0,1] [1,1] [2,1], [0,2] [1,2] [2,2]
         // 대각선 [0,0] [1,1] [2,2], [0,2] [1,1] [2,0]
         
-        for i in 0..2 {
+        for i in 0..<3 {
             // 가로
             if board[i][0] != .empty && board[i][0] == board[i][1] && board[i][1] == board[i][2] {
                 return board[i][0]
@@ -70,6 +82,11 @@ class TicTacToe: GameState {
     
     func printBoard() {
         // 보드를 출력하는 코드
+        print("---------------------------\n")
+//        for row in board {
+//            
+//        }
+        print("---------------------------\n")
     }
     
 }
@@ -83,10 +100,12 @@ while true {
     print("행 입력 (0-2)('q' 입력시 종료): ", terminator: "")
     let rowInput = readLine() ?? ""
     if rowInput == "q" { break }
+    // guard let row = Int(rowInput) else { continue } .. Add_1
     guard let row = Int(rowInput), (0...2).contains(row) else {
         print("잘못된 입력입니다. 0, 1, 2중 하나를 다시 입력하세요.")
         continue
     }
+    // 항복 surrend 추가 해보기
     
     print("열 입력 (0-2)('q' 입력시 종료): ", terminator: "")
     let colInput = readLine() ?? ""
@@ -96,12 +115,13 @@ while true {
         continue
     }
     
-    // 위치에 중복으로 둔 경우
+    // 중복으로 둔 경우
     if !game.makeMove(row: row, column: column) {
         print("잘못된 위치입니다. 다시 시도해주세요.")
         continue
     }
     
+    // 게임 결과
     if let winner = game.checkWinner() {
         game.printBoard()
         if winner == .empty {
