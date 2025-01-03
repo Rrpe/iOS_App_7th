d5243697addbce49ae1bceab1a1e050e3f40e95d
Junsoo Kim <kel_777@naver.com>
2025-01-03 11:45:34 +0900

    TicTacToe Game: 게임루프 수정, 승리조건 추가

--- 02-Playgrounds/TicTacToe.swift
+++ 02-Playgrounds/TicTacToe.swift
@@ -9,14 +9,14 @@ enum Piece {
 protocol GameState {
     var currentPlayer: Piece { get }
     // [[Piece]] 는 타입별칭을 만들 수 있다.
-    var board: [[Piece]] { get }
+    var board: [[Piece]] { get } // 2차원 배열
     func isValidMove(row: Int, column: Int) -> Bool
 }
 
 // 게임 로직을 담당하는 클래스
 class TicTacToe: GameState {
     var board: [[Piece]]
-    var currentPlayer: Piece
+    var currentPlayer: Piece // 플레이어 설정
     
     init() {
         board = [[.empty, .empty, .empty],
@@ -25,6 +25,7 @@ class TicTacToe: GameState {
         currentPlayer = .x // x가 첫 번째 플레이어
     }
     
+    // 유효한 움직임인지 확인
     func isValidMove(row: Int, column: Int) -> Bool {
         // TODO: isValidMove 메서드 구현
         return false
@@ -37,13 +38,40 @@ class TicTacToe: GameState {
     }
     
     func checkWinner() -> Piece? {
+        // 승리조건
+        // 가로 [0,0] [0,1] [0,2], [1,0] [1,1] [1,2], [2,0] [2,1] [2,2]
+        // 세로 [0,0] [1,0] [2,0], [0,1] [1,1] [2,1], [0,2] [1,2] [2,2]
+        // 대각선 [0,0] [1,1] [2,2], [0,2] [1,1] [2,0]
         
+        for i in 0..2 {
+            // 가로
+            if board[i][0] != .empty && board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+                return board[i][0]
+            }
+            //세로
+            if board[0][i] != .empty && board[0][i] == board[1][i] && board[1][i] == board[2][i] {
+                return board[0][i]
+            }
+        }
+            
+        // 대각선 \
+        if board[0][0] != .empty && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+            return board[0][0]
+        }
+        // 대각선 /
+        if board[0][2] != .empty && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+            return board[0][2]
+        }
+        
+        // 무승부
+        // .empty칸이 있으면 아직 진행중 <-> .empty칸이 없으면 게임이 끝났음애도 승부가 안났으니 무승부
         return nil
     }
     
     func printBoard() {
         // 보드를 출력하는 코드
     }
+    
 }
 
 var game = TicTacToe()
@@ -55,12 +83,20 @@ while true {
     print("행 입력 (0-2)('q' 입력시 종료): ", terminator: "")
     let rowInput = readLine() ?? ""
     if rowInput == "q" { break }
-    guard let row = Int(rowInput) else { continue }
+    guard let row = Int(rowInput), (0...2).contains(row) else {
+        print("잘못된 입력입니다. 0, 1, 2중 하나를 다시 입력하세요.")
+        continue
+    }
     
     print("열 입력 (0-2)('q' 입력시 종료): ", terminator: "")
     let colInput = readLine() ?? ""
     if colInput == "q" { break }
-    guard let column = Int(colInput) else { continue }
+    guard let column = Int(colInput), (0...2).contains(column) else {
+        print("잘못된 입력입니다. 0, 1, 2중 하나를 다시 입력하세요.")
+        continue
+    }
+    
+    // 위치에 중복으로 둔 경우
     if !game.makeMove(row: row, column: column) {
         print("잘못된 위치입니다. 다시 시도해주세요.")
         continue
@@ -71,7 +107,7 @@ while true {
         if winner == .empty {
             print("무승부")
         } else {
-            print("\(winner) 승리")
+            print("'\(winner)'플레이어 승리!!! Winner Winner Chicken Dinner!!!")
         }
         break
     }
