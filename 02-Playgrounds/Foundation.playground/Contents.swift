40239a734564287b99776b60b7bfeb10bafea40f
Junsoo Kim <kel_777@naver.com>
2025-01-06 11:36:23 +0900

    Foundation 라이브러리 사용: Calendar, TimeZone, Datacomponents, DateComponentFormatter

--- 02-Playgrounds/Foundation.playground/Contents.swift
+++ 02-Playgrounds/Foundation.playground/Contents.swift
@@ -29,3 +29,32 @@ func howLongUntilHalloween() -> String {
 
 let timeUntilHalloween = howLongUntilHalloween()
 print("할로윈까지 \(timeUntilHalloween) 남았습니다.")
+
+func trumpInauguration() -> String {
+    let calendar = Calendar.current
+    let timeZone = TimeZone.current
+    
+    let now = Date()
+    
+    let yearOfTrumpInauguration = calendar.component(.year, from: now)
+    
+    var components = DateComponents(calendar: calendar, timeZone: timeZone, year: yearOfTrumpInauguration, month: 1, day: 20, hour: 0, minute: 0, second: 0)
+    
+    guard var trumpInauguration = components.date else {
+        print(" trump inauguration date를 가져올 수 없습니다.")
+        return ""
+    }
+    
+    if trumpInauguration < now {
+        trumpInauguration = components.date!
+    }
+    
+    let componentFormatter = DateComponentsFormatter()
+    componentFormatter.unitsStyle = .full
+    componentFormatter.allowedUnits = [.year, .month, .day, .hour, .minute, .second]
+    
+    return componentFormatter.string(from: now, to: trumpInauguration)!
+}
+
+let timeUntilTrumpInauguration = trumpInauguration()
+print(" 트럼프 취임식까지 \(timeUntilTrumpInauguration) 남았습니다.")
