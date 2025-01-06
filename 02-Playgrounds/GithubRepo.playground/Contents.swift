e258e3e92ce1b9e668d7398753cebaa82573cd36
Junsoo Kim <kel_777@naver.com>
2025-01-06 15:30:49 +0900

    JSON 요청과 비동기 함수의 콜백(CompletionHandler)

--- 02-Playgrounds/GithubRepo.playground/Contents.swift
+++ 02-Playgrounds/GithubRepo.playground/Contents.swift
@@ -7,6 +7,11 @@ enum ResponsError: Error {
     case unexpectedResponseStructure
 }
 
+/// @escaping : 비동기 작업에서 클로저가 함수의 실행이 종료된 후에도 호출이 될 수 있음을 나타냄
+/// [[String: Any]]? : 배열로 구성된 JSON 데이터를 나타냄. ?를 써서 옵셔널 타입 선언
+/// -> Void : 클로저 결과만 전달하고 아무 값도 반환 하지 않음을 나타냄
+/// async : 함수가 비동기 작업을 포함한다는 것을 나타냄. await 키워드 호출 필수
+/// completionHandler : 비동기 작업 결과를 전달하기 위해 사용됨
 func fetchRepos(forUsername username: String,
                 completionHandler: @escaping ([[String: Any]]?, ResponsError?) -> Void) async {
     let urlString = "https://api.github.com/users/\(username)/repos"
@@ -33,9 +38,9 @@ func fetchRepos(forUsername username: String,
 }
 
 Task {
-    await fetchRepos(forUsername: "apple") { repos, error in
+    await fetchRepos(forUsername: "Rrpe") { repos, error in
         if let error {
-            print("error!")
+            print("Error: \(error)")
             return
         }
         print("Repos: \(repos!.count)")
