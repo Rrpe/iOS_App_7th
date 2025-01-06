bace9d1b5b1c556d284951d6e934f19d3d669cd0
Junsoo Kim <kel_777@naver.com>
2025-01-06 11:07:49 +0900

    중첩타입과 네임스페이스

---
+++ 02-Playgrounds/NestedType.playground/Contents.swift
@@ -0,0 +1,44 @@
+
+class Device {
+    enum Category {
+        case watch
+        case phone
+        case tablet
+    }
+    
+    enum Orientation {
+        case portrait
+        case portariaUpsideDown
+        case landscapeLeft
+        case landscapeRight
+    }
+    
+    let category: Category
+    let orientation: Orientation = .portrait
+    
+    init(category: Category) {
+        self.category = category
+    }
+}
+
+let device = Device(category: .phone)
+let desiredOrientation: Device.Orientation = .portrait
+let phoneHasDesiredOrientation: Bool = device.orientation == desiredOrientation
+
+struct UserInterface {
+    struct Version {
+        let major: Int
+        let minor: Int
+        let patch: Int
+    }
+    
+    enum Orientation {
+        case portrait
+        case landscape
+    }
+    
+    let version: Version
+    let orientation: Orientation
+}
+
+func uiOrientation(for deviceOrientation:)
