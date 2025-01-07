0d3f6d8fc759a2a331bcc7dd4e1a8efbf3fa3313
Junsoo Kim <kel_777@naver.com>
2025-01-07 15:13:05 +0900

    Function: 가변 매개변수, inout, 함수 타입 변수, 클로저

--- 04-SwiftUI/Functions.playground/Contents.swift
+++ 04-SwiftUI/Functions.playground/Contents.swift
@@ -26,3 +26,43 @@ let message3 = buildMessageFor(count: 100)
 let message4 = buildMessageFor("아무개", count: 100)
 print(message3)
 print(message4)
+
+func displayStrings(_ strings: String...) {
+    for string in strings {
+        print(string)
+    }
+}
+
+displayStrings("Hello", "World", "Swift", "Programming")
+
+func displayStrings(_ strings: String..., counts: Int...) {
+    for string in strings {
+        print(string)
+    }
+    print("count: \(counts)")
+}
+displayStrings("Hello", "World", "Swift", "Programming", counts: 1, 2, 3)
+
+// inout 키워드: 파라미터 값을 외부에도 적용해주는 키워드
+func doubleValue(_ value: inout Int)/* -> Void */ {
+    value *= 2
+    print(value)
+}
+
+var myValue: Int = 10
+doubleValue(&myValue)
+print(myValue)
+
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+print(add(2, 3))
+
+// 변수의 타입: 함수 타입 = 클로저 타입 '(매개변수) -> (반환값)'
+let mathFunction: (Int, Int) -> Int = add
+print(mathFunction(10, 20))
+
+let multiply = { (a: Int, b: Int) -> Int in
+    return a * b
+}
+print(multiply(2, 3))
