09fd02d47abdd89a7760dd30b13392ccd3efb509
Junsoo Kim <kel_777@naver.com>
2025-01-09 14:31:33 +0900

    Clear

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -10,49 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
-            }
-            ZStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
-            }
-            // 복잡한 중첩 레이아웃 예시
-            Text("Financial Results").font(.title)
-            HStack {
-                Text("Q1 Sales").font(.headline)
-                VStack {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                VStack {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-            }
-            // 정렬, 여백
-            Text("Financial Results").font(.title).padding(10)
-            HStack(alignment: .top) {
-                Text("Qi Sales").font(.headline)
-                Spacer()
-                // alignment: .trailing, .center, .leading
-                VStack(alignment: .trailing) {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-            }
+            
             
         } // VStack
     }
