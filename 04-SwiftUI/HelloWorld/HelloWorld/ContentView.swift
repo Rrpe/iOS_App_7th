765f75a701444931601eaf7c75c75ea37b48d4a4
Junsoo Kim <kel_777@naver.com>
2025-01-10 09:23:19 +0900

    Picker, Slider, animation modifier

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -40,13 +40,20 @@ struct ContentView: View {
                     .padding()
                 
                 // 피커, 색상 선택
-                Picker(selection: $colorIndex, label: Text("Color")) {
-                    ForEach(0 ..< colornames.count, id: \.self) {
-                        Text(colornames[$0])
-                            .foregroundStyle(colors[$0])
+//                List {
+                // 기본 피커 스타일을 ListItem 으로 사용할 경우 Label 이 표시됨
+                // 기본 스타일 에서는 레이블이 표시되지 않음.
+                // ( 접근성을 통한 VoiceOver 음성 안내에서만 활용됨 )
+                    Picker("Color", selection: $colorIndex ) {
+//                    Picker(selection: $colorIndex, label: Text("Color")) {
+                        ForEach(0 ..< colornames.count, id: \.self) {
+                            Text(colornames[$0])
+                                .foregroundStyle(colors[$0])
+                        }
                     }
-                }.pickerStyle(.wheel)
-                .padding()
+                    .pickerStyle(.wheel)
+                    .padding()
+//                } // List
             }
         
         } // VStack
