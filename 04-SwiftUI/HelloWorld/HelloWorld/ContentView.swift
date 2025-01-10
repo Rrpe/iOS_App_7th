ba788c4b9a4425e2af880eb38778ff744a8942e5
Junsoo Kim <kel_777@naver.com>
2025-01-10 09:44:24 +0900

    텝 제스쳐

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -1,61 +1,22 @@
-//
-//  ContentView.swift
-//  SwiftUIDemoAPp
-//
-//  Created by KimJunsoo on 1/9/25.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    var colors: [Color] = [.black, .red, .green, .blue]
-    var colornames = ["Black", "Red", "Green", "Blue"]
-    
-    @State private var colorIndex = 0
-    @State private var rotation: Double = 0.0
-    @State private var text: String = "Welcome to SwiftUI"
+    @State private var isTapped = false
     
     var body: some View {
         VStack {
             
-            VStack {
-                Spacer()
-                
-                // 텍스트 표시 및 애니메이션(회전) 추가
-                Text("Hello, world!")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .rotationEffect(.degrees(rotation))
-                    .animation(.easeInOut(duration: 1), value: rotation)
-                    .foregroundStyle(colors[colorIndex])
-                Spacer()
-                Divider() // 구분선
-                
-                // 슬라이더, 텍스트 회전 각도 설정
-                Slider(value: $rotation, in: 0...360, step: 0.1)
+            Text("Tap me!")
+                .font(.largeTitle)
                 .padding()
-                
-                TextField("Enter text here", text: $text)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                
-                // 피커, 색상 선택
-//                List {
-                // 기본 피커 스타일을 ListItem 으로 사용할 경우 Label 이 표시됨
-                // 기본 스타일 에서는 레이블이 표시되지 않음.
-                // ( 접근성을 통한 VoiceOver 음성 안내에서만 활용됨 )
-                    Picker("Color", selection: $colorIndex ) {
-//                    Picker(selection: $colorIndex, label: Text("Color")) {
-                        ForEach(0 ..< colornames.count, id: \.self) {
-                            Text(colornames[$0])
-                                .foregroundStyle(colors[$0])
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    .padding()
-//                } // List
-            }
-        
+                .background(isTapped ? Color.green : Color.red)
+                .animation(.default, value: isTapped)
+                // Tap 카운트를 이용해 실행시킬 탭 횟수를 조절할 수 있음
+                .onTapGesture(count: 3) {
+                    isTapped.toggle()
+                }
+            
         } // VStack
     } // body View
 } // Content View
