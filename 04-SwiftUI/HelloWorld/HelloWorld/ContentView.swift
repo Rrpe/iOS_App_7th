58c4afe5b335e256bed2a88af2ba13a1c4df911c
Junsoo Kim <kel_777@naver.com>
2025-01-10 11:48:00 +0900

    Swift UI : actor

--- 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ 04-SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -1,41 +1,48 @@
 
 import SwiftUI
 
-enum DurationError: Error {
-    case tooLong
-    case tooShort
+// 구조화된 비동기 클래스
+actor BuildMessage {
+    var message: String = ""
+    let greeting = "Hello"
+    
+    func setName(name: String) {
+        self.message = "\(greeting), \(name)"
+    }
+    
+    // 격리되지 않은 함수
+    nonisolated func getGreeting() -> String {
+        return greeting
+    }
 }
 
+@MainActor
 struct ContentView: View {
+    var builder = BuildMessage()
+    
     var body: some View {
-        Button(action: {
-            Task {
-                await doSomething()
-            }
-        }) {
-            Text("Do Something")
-        }
+      
+        Text("Hello, World!")
     }
     
-    func doSomething() async {
-        var timeStamps: [Int: Date] = [:]
-        timeStamps = await withTaskGroup(of: (Int, Date).self) { group in
-            for i in 1...5 {
-                group.addTask {
-                    return (i, await takesTooLong())
-                }
-            }
-            var results: [Int: Date] = [:]
-            for await (task, date) in group {
-                results[task] = date
-            }
-            return results
-        }
+    // 액터의 메서드를 호출하거나 프로퍼티에 접근할 때는 'await' 키워드를 사용해야 합니다.
+    func someFunction() async {
+        await builder.setName(name: "Jane Smith")
+        let message = await builder.message
+        print(message)
     }
     
-    func takesTooLong() async -> Date {
-        sleep(5)
-        return Date()
+    @MainActor
+    func asyncFunction() async {
+        await builder.setName(name: "Jane Smith")
+        let greeting = builder.getGreeting()
+        print(greeting)
+    }
+    
+    func syncFunction() {
+//        builder.setName(name: "Jane Smith") // Swift 6 부터 사용불가
+        let greeting = builder.getGreeting()
+        print(greeting)
     }
 }
 
