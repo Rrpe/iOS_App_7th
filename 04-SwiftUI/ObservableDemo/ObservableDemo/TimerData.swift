069609a9880621a3d45941726e9c703f725f8bbb
Junsoo Kim <kel_777@naver.com>
2025-01-13 13:24:11 +0900

    unowned self, deinit

--- 04-SwiftUI/ObservableDemo/ObservableDemo/TimerData.swift
+++ 04-SwiftUI/ObservableDemo/ObservableDemo/TimerData.swift
@@ -21,10 +21,18 @@ class TimerData: ObservableObject {
         //                                     repeats: true)
                 
         // [weak self] 는 변수 캡쳐 시 ARC 카운트가 증가하지 않도록 해서, 메모리 누수를 막는다.
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self]_ in
-            self?.timerDidFire()
-                
+//        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self]_ in
+//            self?.timerDidFire()
+//        }
+        
+        // [unowned self] 는 강제 언래핑된 self 키워드, 조심해서 사용해야함.
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [unowned self] _ in
+            self.timerDidFire()
         }
+    }
+    
+    deinit {
+        timer?.invalidate()
     }
     
     /*@objc 기존 셀렉터 방식의 호출: 반드시 함수 정의시에 @objc 키워드 사용 */ func timerDidFire() {
