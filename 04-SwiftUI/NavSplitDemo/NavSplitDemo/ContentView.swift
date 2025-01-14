4a451539418bdeb6eca9de442f65ac37d66ea2a7
Junsoo Kim <kel_777@naver.com>
2025-01-14 15:25:34 +0900

    Navigition Split View

--- 04-SwiftUI/NavSplitDemo/NavSplitDemo/ContentView.swift
+++ 04-SwiftUI/NavSplitDemo/NavSplitDemo/ContentView.swift
@@ -26,17 +26,36 @@ struct ContentView: View {
                                                           "cloud.drizzle"])
     ]
     @State private var selectedCategory: IconCategory?
+    @State private var selectedImage: String?
+    @State private var columnVisibility = NavigationSplitViewVisibility.all
     
     var body: some View {
-        NavigationSplitView {
+        NavigationSplitView(columnVisibility: $columnVisibility) {
             List(categories, selection: $selectedCategory) { category in
                 Text(category.categoryName).tag(category)
                 
             }
         } content: {
-            
+            if let selectedCategory {
+                List(selectedCategory.images, id: \.self, selection: $selectedImage) { image in
+                    HStack {
+                        Image(systemName: image)
+                        Text(image)
+                    }.tag(image)
+                    
+                }
+            } else {
+                Text("Select a category")
+            }
         } detail: {
-            
+            if let selectedImage {
+                Image(systemName: selectedImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+            } else {
+                Text("Select an image")
+            }
         }
     }
 }
