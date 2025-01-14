c0e97740c524764e1a639cbe2fd06fd9aa1fd55f
Junsoo Kim <kel_777@naver.com>
2025-01-14 15:40:46 +0900

    Add SettingsView

---
+++ 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  OutlineGroupDemo
+//
+//  Created by KimJunsoo on 1/14/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
