fd6e5a5bbd2dd4fda0b36166dbca16a2cde6ec69
Junsoo Kim <kel_777@naver.com>
2025-01-14 15:47:24 +0900

    Disclosure Group List

--- 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/SettingsView.swift
+++ 04-SwiftUI/OutlineGroupDemo/OutlineGroupDemo/SettingsView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var hybridState: Bool = false
+    @State private var electricState: Bool = true
+    @State private var fuelCellState: Bool = false
+    @State private var inversionState: Bool = true
+    @State private var filtersExpanded: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            DisclosureGroup(isExpanded: $filtersExpanded) {
+                ToggleControl(title: "Hybrid Cars", state: $hybridState)
+                ToggleControl(title: "Electric Cars", state: $electricState)
+                ToggleControl(title: "Fuel Cell Cars", state: $fuelCellState)
+            } label: {
+                Label("Categories Filters", systemImage: "car.2.fill")
+            }
+            
+            DisclosureGroup {
+                ColorControl(color: .red, label: "Background Color")
+                ColorControl(color: .blue, label: "Foreground Color")
+                ToggleControl(title: "Color Inversion", state: $inversionState)
+            } label: {
+                Label("Color Settings", systemImage: "scribble.variable")
+            }
+        }
     }
 }
 
 #Preview {
     SettingsView()
+}
+
+struct ColorControl: View {
+    var color: Color
+    var label: String
+    var body: some View {
+        HStack {
+            Text(label)
+            Spacer()
+            Rectangle()
+                .fill(color)
+                .frame(width: 30, height: 30)
+        }
+        .padding(.leading)
+    }
+}
+
+struct ToggleControl: View {
+    var title: String
+    @Binding var state: Bool
+    
+    var body: some View {
+        Toggle(title, isOn: $state)
+            .padding(.leading)
+    }
 }
