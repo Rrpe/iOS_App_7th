35f997349653f8f5ed7e456559a90083810b9c87
Junsoo Kim <kel_777@naver.com>
2025-01-14 14:46:58 +0900

    네비게이션 경로 추가

---
+++ 04-SwiftUI/ListNavDemo/ListNavDemo/AddNewCar.swift
@@ -0,0 +1,66 @@
+//
+//  AddNewCar.swift
+//  ListNavDemo
+//
+//  Created by KimJunsoo on 1/14/25.
+//
+
+import SwiftUI
+
+struct AddNewCar: View {
+//    @State var carStore = CarStore()
+    var carStore: CarStore
+    @Binding var path: NavigationPath
+    @State private var isHybrid: Bool = false
+    @State private var name: String = ""
+    @State private var description: String = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Car Details")) {
+                Image(systemName: "car.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                DataInput(title: "Model", userInput: $name)
+                DataInput(title: "Description", userInput: $description)
+                
+                Toggle(isOn: $isHybrid) {
+                    Text("Hybrid").font(.headline)
+                }
+                .padding()
+            }
+            
+            Button(action: addNewCar) {
+                Text("Add Car")
+            }
+        }
+    }
+    
+    func addNewCar() {
+        let newCar = Car(id: UUID().uuidString, name: name, description: description, imageName: "tesla_model_3", isHybrid: isHybrid)
+        
+        carStore.cars.append(newCar)
+        path.removeLast()
+    }
+}
+
+//#Preview {
+//    AddNewCar(carStroe: CarStore(), path: .init(NavigationPath.self))
+//}
+
+struct DataInput: View {
+    var title: String
+    @Binding var userInput: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.headline)
+            
+            TextField("Enter \(title)", text: $userInput)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+    }
+}
