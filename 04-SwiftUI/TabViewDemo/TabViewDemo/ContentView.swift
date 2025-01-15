c6cd67f97e01f8b071ee3ecb957a15d8cae98f0b
Junsoo Kim <kel_777@naver.com>
2025-01-15 17:47:20 +0900

    TabView UIKit사용해보기, 주석추가, tabViewStyle 스타일 적용 방식 확인

--- 04-SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
+++ 04-SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
@@ -10,14 +10,23 @@ import SwiftUI
 struct ContentView: View {
     @State var selection: Int = 1
     
+    init() {
+            // UIPageControl의 외형 설정
+            UIPageControl.appearance().currentPageIndicatorTintColor = UIColor.black
+            UIPageControl.appearance().pageIndicatorTintColor = UIColor.red
+        }
+    
     var body: some View {
+        //selection 값에 따라 화면 전환_ 시작 탭 정할 수도 있음.(현재는 0번)
         TabView(selection: $selection) {
             Text("First Content View")
+                // 하단에 생성)탭바에 있는 아이콘
                 .tabItem {
                     Image(systemName: "1.circle")
                     Text("Screen One")
                 }
                 .tag(0)
+                .badge("!")
             Text("Second Content View")
                 .tabItem {
                     Image(systemName: "2.circle")
@@ -30,9 +39,18 @@ struct ContentView: View {
                     Text("Screen Three")
                 }
                 .tag(2)
+                .badge(10) // 알림 추가
         }
         .font(.largeTitle)
-        .tabViewStyle(PageTabViewStyle())
+        .border(Color.gray)
+        .foregroundStyle(.white)
+        .background(Color.black)
+        .frame(height: 400)
+        // tabItem인디게이터화 및 뷰(화면)슬라이드 가능
+        // 앱 상단 광고(?) 페이지에서 자주 본듯함
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+        .padding()
+        
         HStack {
             Button("1") {
                 selection = 0
@@ -47,6 +65,14 @@ struct ContentView: View {
             }
             .disabled(selection == 2)
         }
+        /*HStack(spacing: 10) {
+            ForEach(0..<3) { index in
+                Circle()
+                    .frame(width: 15, height: 15)
+                    .foregroundColor(selection == index ? .blue : .red)
+            }
+        }
+        .padding()*/
     }
 }
 
