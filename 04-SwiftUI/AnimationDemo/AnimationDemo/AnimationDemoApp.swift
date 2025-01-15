99b59bfa20b447ae6e221fc0512ff7f1e9a5aae9
Junsoo Kim <kel_777@naver.com>
2025-01-15 14:04:47 +0900

    Animation

---
+++ 04-SwiftUI/AnimationDemo/AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by KimJunsoo on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
