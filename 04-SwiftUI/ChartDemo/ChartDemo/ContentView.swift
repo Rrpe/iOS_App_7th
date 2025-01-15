ce22f91d416a7af7ec9a07a6b59e87320e23d4a2
Junsoo Kim <kel_777@naver.com>
2025-01-15 15:50:32 +0900

    차트, 차트배경, 보간법

--- 04-SwiftUI/ChartDemo/ChartDemo/ContentView.swift
+++ 04-SwiftUI/ChartDemo/ChartDemo/ContentView.swift
@@ -6,16 +6,54 @@
 //
 
 import SwiftUI
+import Charts
+
+struct MonthlyTemp: Identifiable {
+    var id = UUID()
+    var month: String
+    var degrees: Int
+    var year: String
+}
+
+let tempData: [MonthlyTemp] = [
+    MonthlyTemp(month: "Jan", degrees: 50, year: "2021"),
+    MonthlyTemp(month: "Feb", degrees: 43, year: "2021"),
+    MonthlyTemp(month: "Mar", degrees: 61, year: "2021"),
+    MonthlyTemp(month: "Jan", degrees: 30, year: "2022"),
+    MonthlyTemp(month: "Feb", degrees: 38, year: "2022"),
+    MonthlyTemp(month: "Mar", degrees: 29, year: "2022")
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart(tempData) { data in
+//            AreaMark(
+            LineMark(
+                x: .value("Month", data.month),
+                y: .value("Temp", data.degrees)
+            )
+            .interpolationMethod(.linear)
+            .foregroundStyle(by: .value("Year", data.year))
+            .symbol(by: .value("Year", data.year))
+        }
+        .chartPlotStyle { plotArea in
+            plotArea.background(.gray.opacity(0.3))
         }
         .padding()
+        /*Chart {
+            AreaMark (
+                x: .value("Month", "Jan"),
+                y: .value("Temp", 50)
+            )
+            AreaMark (
+                x: .value("Month", "Feb"),
+                y: .value("Temp", 43)
+            )
+            AreaMark (
+                x: .value("Month", "Mar"),
+                y: .value("Temp", 61)
+            )
+        }*/
     }
 }
 
