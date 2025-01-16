61007209ee44e35daefd6daea6e3aaa93185fdaf
Junsoo Kim <kel_777@naver.com>
2025-01-16 10:36:23 +0900

    필터 적용

--- 04-SwiftUI/ImageDocDemo/ImageDocDemo/ContentView.swift
+++ 04-SwiftUI/ImageDocDemo/ImageDocDemo/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @Binding var document: ImageDocDemoDocument
+    @State private var ciFilter = CIFilter.sepiaTone()
 
+    let context = CIContext()
+    
     var body: some View {
         VStack {
             Image(uiImage: document.image)
@@ -18,13 +23,27 @@ struct ContentView: View {
                 .padding()
             
             Button(action: {
-                
+                filterImage()
             }, label: {
                 Text("Filter Image")
             })
             .padding()
         }
     }
+    
+    func filterImage() {
+        ciFilter.intensity = Float(1.0)
+        
+        let ciImage = CIImage(image: document.image)
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        guard let outputImage = ciFilter.outputImage else { return }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            document.image = UIImage(cgImage: cgImage)
+        }
+    }
+    
 }
 
 #Preview {
