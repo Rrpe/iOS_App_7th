77fbaa157afea2802363cb6bf7e904d9d86412f5
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-11 15:24:54 +0900

    Travel 클래스에 거리 변환 메서드 추가 및 테스트 파일 경로 수정

--- 05-Dart And Flutter/06-test/test/travel_test.dart
+++ 05-Dart And Flutter/06-test/test/travel_test.dart
@@ -1,8 +1,6 @@
 import 'package:test_example/test_dart_sample.dart';
 import 'package:test/test.dart';
 
-import '../test_dart_sample.dart';
-
 void main() {
   test('Travel Distance', () {
     var distance = 10.0;
