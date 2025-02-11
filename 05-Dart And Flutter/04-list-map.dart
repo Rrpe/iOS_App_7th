78062d3f326221148773c704ecccd817b474c820
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-11 09:12:25 +0900

    리스트에 요소 추가 및 출력 기능 구현

---
+++ 05-Dart And Flutter/04-list-map.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<String> months = [
+    'January',
+    'February',
+    'March',
+  ];
+
+  months.add('April');
+
+  months.forEach(print);
+
+  print(months[0]);
+
+  print(months.length);
+}
