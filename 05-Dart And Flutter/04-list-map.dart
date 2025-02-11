b179fcdb59b826b7b6ac4cf6e84d07899b774965
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-11 09:25:46 +0900

    리스트 심화: 요소 추가, 제거 및 필터링 기능 구현

--- 05-Dart And Flutter/04-list-map.dart
+++ 05-Dart And Flutter/04-list-map.dart
@@ -12,4 +12,20 @@ void main() {
   print(months[0]);
 
   print(months.length);
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  numbers.insert(2, 10);
+  print(numbers);
+  numbers.remove(4);
+  print(numbers);
+  numbers.removeAt(2);
+  print(numbers);
+  // 조건에 따라 필터링
+  List<int> evenNumbers = numbers.where((number) => number % 2 == 0).toList();
+  print(evenNumbers); // 2 출력
+
+  // 각 요소를 제곱하여 새로운 리스트 생성
+  List<int> squaredNumbers = numbers.map((number) => number * number).toList();
+  print(squaredNumbers); // 1, 4, 9, 25 출력
 }
