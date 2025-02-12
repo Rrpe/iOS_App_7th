d994144ebc69e336fc0dfb744e77e23df0a9b00f
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-12 15:15:51 +0900

    구글 폰트 적용

--- 05-Dart And Flutter/08-assets/lib/main.dart
+++ 05-Dart And Flutter/08-assets/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,22 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Text('Hello World'),
-    );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Scaffold(
+            appBar: AppBar(title: Text('Google Fonts 데모')),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('헬로 월드!', style: TextStyle(fontSize: 24)),
+                Text('헬로 월드!',
+                    // style: GoogleFonts.nanumGothic(
+                    style: GoogleFonts.nanumPenScript(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.blue)),
+              ],
+            ))));
   }
 }
