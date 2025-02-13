9f2031c840589fdf072923cc3bfe7f901b214297
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-13 15:15:08 +0900

    Flutter GridView

--- 05-Dart And Flutter/list_navigation/lib/main.dart
+++ 05-Dart And Flutter/list_navigation/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:list_navigation/my_list_view.dart';
 import 'package:list_navigation/my_sliver_list_view.dart';
+import 'package:list_navigation/my_grid_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MySliverList(),
+      home: const MyGridView(),
     );
   }
 }
