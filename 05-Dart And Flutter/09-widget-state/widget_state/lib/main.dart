7d4cfc26333d27c9c09c4a1f82e3f0af0fb8bae6
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-13 10:47:20 +0900

    구글 폰트 추가

--- 05-Dart And Flutter/09-widget-state/widget_state/lib/main.dart
+++ 05-Dart And Flutter/09-widget-state/widget_state/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+// 메인 함수
 void main() {
+  // runApp 함수를 호출하여 MyApp 위젯을 실행
   runApp(const MyApp());
 }
 
-// MyApp 위젯: StatelessWidget 상태관리가 필요하지 않는 단순한 형태의 위젯
+// MyApp 위젯: StatelessWidget 상태관리가 필요하지 않은 단순한 형태의 위젯
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,14 +15,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Google Fonts Demo',
+      debugShowCheckedModeBanner: false,
       // 테마 설정
       theme: ThemeData(
-        // 색상 스키마 설정
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // 머테리얼 버전 설정
-        useMaterial3: true,
-      ),
+          // 기본 색상 스키마 설정
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+            bodyLarge:
+                GoogleFonts.aBeeZee(fontSize: 20, color: Colors.deepOrange),
+            bodyMedium:
+                GoogleFonts.aBeeZee(fontSize: 18, color: Colors.grey[700]),
+          )),
+      // 홈 페이지 설정
+      // 타이틀 파라미터 전달
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -29,6 +38,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+  // title 변수: 홈페이지의 타이틀을 저장하는 런타임 상수
   final String title;
 
   // createState 함수를 호출하여 MyHomePageState 객체를 생성
@@ -41,29 +51,32 @@ class _MyHomePageState extends State<MyHomePage> {
   // build 함수: 위젯을 생성하는 함수
   @override
   Widget build(BuildContext context) {
+    // Scaffold 위젯: 머테리얼 디자인의 기본 레이아웃 구조를 제공
     return Scaffold(
       backgroundColor: Colors.grey,
+      // body 속성에 Center 위젯을 사용하여 화면 중앙에 컨텐츠를 배치
       body: Center(
         child: Container(
             width: 300,
             height: 380,
             color: Colors.white,
             child: ListView.builder(
-                itemCount: 30,
-                itemBuilder: (context, index) {
-                  return _buildListItem();
-                })),
+              itemCount: 30,
+              itemBuilder: (context, index) {
+                return _buildListItem();
+              },
+            )),
       ),
     );
   }
 
   Widget _buildListItem() {
     return ListTile(
-      title: Text('Hello, World!',
-          style: TextStyle(fontSize: 20, color: Colors.indigo)),
-      subtitle: Text('Dart & Flutter',
-          style: TextStyle(fontSize: 16, color: Colors.deepPurple)),
+      title: Text('Hello, World!'),
+      subtitle: Text('Dart & Flutter'),
       leading: Image.network('https://picsum.photos/120/60'),
+      titleTextStyle: Theme.of(context).textTheme.bodyLarge,
+      subtitleTextStyle: Theme.of(context).textTheme.bodyMedium,
     );
   }
 }
