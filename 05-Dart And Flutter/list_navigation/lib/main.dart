3a39f99d7a3904234920c149592793cdc79f6260
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-13 14:02:09 +0900

    Sliver List View ( 슬리버 리스트 뷰 )

--- 05-Dart And Flutter/list_navigation/lib/main.dart
+++ 05-Dart And Flutter/list_navigation/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:list_navigation/my_list_view.dart';
+import 'package:list_navigation/my_sliver_list_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyListView(),
+      home: const MySliverList(),
     );
   }
 }
