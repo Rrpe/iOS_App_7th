109af17d71b17571864a9074062ac86f39f1eb1d
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-13 13:22:32 +0900

    화면 ListView로 변경

--- 05-Dart And Flutter/list_navigation/lib/main.dart
+++ 05-Dart And Flutter/list_navigation/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -21,11 +20,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyListView() extends StatelessWidget {
+class ListDataItems {
+  final List<String> monthItems = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+}
+
+class MyListView extends StatelessWidget {
   const MyListView({super.key});
-  
- @override
+
+  @override
   Widget build(BuildContext context) {
-    return ;
+    final ListDataItems item = ListDataItems();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListView"),
+      ),
+      body: ListView.builder(
+        itemCount: item.monthItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(item.monthItems[index]),
+          );
+        },
+      ),
+    );
   }
 }
