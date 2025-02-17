2b0c2360630c976582cf895181141ce97121833a
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-17 09:45:11 +0900

    Navigator Demo 위젯 추가

--- 05-Dart And Flutter/list_navigation/lib/main.dart
+++ 05-Dart And Flutter/list_navigation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:list_navigation/navigator_demo.dart';
 
 import 'package:list_navigation/shared_preference_demo.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SharedPreferenceDemo(),
+      home: NavigatorDemo(),
     );
   }
 }
