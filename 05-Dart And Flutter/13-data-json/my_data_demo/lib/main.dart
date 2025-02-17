a8c95b822d4cc36aa64f9671bd74519049ed9f26
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-17 14:05:48 +0900

    Add JsonFile & pubspec.yaml assets path setting

--- 05-Dart And Flutter/13-data-json/my_data_demo/lib/main.dart
+++ 05-Dart And Flutter/13-data-json/my_data_demo/lib/main.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyData {
-  final List<String> items = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-}
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
 
-  final MyData data = MyData();
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<List<String>> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = _loadMonths();
+  }
+
+  Future<List<String>> _loadMonths() async {
+    String jsonString = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/example.json');
+    return jsonDecode(jsonString).values.cast<String>().toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,10 +37,19 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('My Data App')),
-        body: ListView.builder(
-          itemCount: data.items.length,
-          itemBuilder: (context, index) {
-            return ListTile(title: Text(MyData().items[index]));
+        body: FutureBuilder(
+          future: futureData,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(data[index]));
+              },
+            );
           },
         ),
       ),
