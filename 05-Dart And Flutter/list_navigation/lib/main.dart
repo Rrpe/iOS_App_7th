5ab233a9731e7856b199df06941d526501094864
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-17 11:20:38 +0900

    Add. Tab Demo, Change Theme Color

--- 05-Dart And Flutter/list_navigation/lib/main.dart
+++ 05-Dart And Flutter/list_navigation/lib/main.dart
@@ -13,8 +13,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        tabBarTheme: const TabBarTheme(
+          labelColor: Colors.amber,
+          indicatorColor: Colors.amber,
+        ),
       ),
       home: TabDemo(),
     );
