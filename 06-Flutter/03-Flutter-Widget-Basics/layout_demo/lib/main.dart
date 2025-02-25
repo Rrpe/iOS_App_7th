12353a022f8d4783b6836fc85c1cef31471ce104
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-25 10:36:48 +0900

    반응형 UI 예제 개선: LayoutBuilder와 Row 위젯을 사용하여 텍스트와 두 개의 위젯을 수직 및 수평으로 배치

--- 06-Flutter/03-Flutter-Widget-Basics/layout_demo/lib/main.dart
+++ 06-Flutter/03-Flutter-Widget-Basics/layout_demo/lib/main.dart
@@ -13,13 +13,37 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(title: const Text('반응형 UI 예제')),
-        body: LayoutBuilder(
-          builder: (context, BoxConstraints constraints) {
-            double fontSize = constraints.maxWidth > 600 ? 32 : 24;
-            return Center(
-              child: Text('반응형 텍스트', style: TextStyle(fontSize: fontSize)),
-            );
-          },
+        body: Column(
+          children: [
+            LayoutBuilder(
+              builder: (context, BoxConstraints constraints) {
+                double fontSize = constraints.maxWidth > 600 ? 32 : 24;
+                return Center(
+                  child: Text('반응형 텍스트', style: TextStyle(fontSize: fontSize)),
+                );
+              },
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2, // 2:1 배율
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.red,
+                    child: Text('위젯 1'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.green,
+                    child: Text('위젯 2'),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
