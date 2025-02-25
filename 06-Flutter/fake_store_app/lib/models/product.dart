24f20e332e0df395870099d72d834ca0d9117b37
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-02-25 14:36:47 +0900

    모델,  API서비스 추가

---
+++ 06-Flutter/fake_store_app/lib/models/product.dart
@@ -0,0 +1,28 @@
+class Product {
+  final String id;
+  final String title;
+  final double price;
+  final String description;
+  final String image;
+  final Map<String, dynamic> rating;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.image,
+    required this.rating,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: (json['price'] as num).toDouble(),
+      description: json['description'],
+      image: json['image'],
+      rating: json['rating'],
+    );
+  }
+}
