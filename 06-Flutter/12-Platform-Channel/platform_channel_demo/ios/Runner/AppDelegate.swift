1fff64e4e957d98aa1d925c40fa9c7d1c1c68808
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-05 11:04:39 +0900

    iOS 앱 플랫폼 채널 추가: 배터리 수준을 가져오는 기능 구현 및 UI 정렬 개선

--- 06-Flutter/12-Platform-Channel/platform_channel_demo/ios/Runner/AppDelegate.swift
+++ 06-Flutter/12-Platform-Channel/platform_channel_demo/ios/Runner/AppDelegate.swift
@@ -3,11 +3,37 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // 안전한 옵셔널 언래핑
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let batteryChannel = FlutterMethodChannel(name: "kr.co.codegrove.platform_channel_demo/battery", binaryMessenger: controller.binaryMessenger)
+        
+        batteryChannel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            guard self != nil else { return }
+            if call.method == "getBatteryLevel" {
+                let device = UIDevice.current
+                device.isBatteryMonitoringEnabled = true  // 🔥 이 부분이 필요함
+                if device.batteryState == UIDevice.BatteryState.unknown {
+#if targetEnvironment(simulator)
+                    result(66) // 시뮬레이터에서는 임의의 배터리 값을 반환
+#else
+                    result(FlutterError(code: "UNAVAILABLE",
+                                        message: "Battery info unavailable",
+                                        details: nil))
+#endif
+                } else {
+                    result(Int(device.batteryLevel * 100))
+                }
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
