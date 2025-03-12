e2f39f12d3165e53caa753b341d6e7ea5b7a102b
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-12 14:14:09 +0900

    SecondViewController와 ViewController 간의 delegate 패턴 구현: 메시지 전달 기능 추가

--- 07-UIKit/HelloWorld/HelloWorld/ViewController.swift
+++ 07-UIKit/HelloWorld/HelloWorld/ViewController.swift
@@ -7,43 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SecondViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("1 ViewController.viewDidLoad()")
+        print("01 ViewController.viewDidLoad()")
         
         setupUI()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("2 ViewController.viewWillAppear()")
-    }
-    
-    override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        print("3 ViewController.viewIsAppearing()")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("4 ViewController.viewDidAppear()")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("5 ViewController.viewWillDisappear()")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("6 ViewController.viewDidDisappear()")
-    }
-    
     func setupUI() {
-        print("7 ViewController.setupUI()")
+        print("07 ViewController.setupUI()")
         let label = UILabel()
         label.text = "Hello, World!"
         label.textAlignment = .center
@@ -62,9 +37,13 @@ class ViewController: UIViewController {
         self.view.addSubview(button)
     }
     
+    func didDismissSecondViewController(message: String) {
+        print("SecondViewController에서 전달받은 메시지: \(message)")
+    }
     
     @objc func goSecond() {
         let secondVC = SecondViewController()
+        secondVC.delegate = self
         self.present(secondVC, animated: true)
     }
 }
