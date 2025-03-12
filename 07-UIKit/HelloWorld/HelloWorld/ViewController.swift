e0a0efe91463255de92106e55a7443e08268b117
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-12 13:26:44 +0900

    UIViewController 개선: UI 설정 및 생명주기 메서드에 로그 추가

--- 07-UIKit/HelloWorld/HelloWorld/ViewController.swift
+++ 07-UIKit/HelloWorld/HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("ViewController.viewDidLoad()")
+        print("1 ViewController.viewDidLoad()")
         
         setupUI()
     }
