81db01df6c5ab76574f9dd66dbd5d9c18ebc55b5
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-13 14:18:11 +0900

    UITableViewController: UIListContentConfiguration 사용

--- 07-UIKit/UIViewDemo/UIViewDemo/FormTableViewController.swift
+++ 07-UIKit/UIViewDemo/UIViewDemo/FormTableViewController.swift
@@ -17,18 +17,18 @@ class FormTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        // section을 4개만 나오게
         return 4
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         switch section {
-        case 0:
+        case 0: // 0번째 section은 1줄
             return 1
-        case 3:
+        case 3: // 3번째 section은 3줄
             return 3
-        default:
+        default: // 나머지 section은 2줄
             return 2
         }
     }
@@ -37,6 +37,11 @@ class FormTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 테이블 뷰 생성
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        // Configure the cell...
+        var config = UIListContentConfiguration.subtitleCell()
+        config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
+        cell.contentConfiguration = config
         
         cell.backgroundColor = .systemGray
 
