14022dacd9c56a276fbe43fe58072203272a1fee
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-13 11:21:13 +0900

    UIKit: UIView -> UIStackView 추가

--- 07-UIKit/UIViewDemo/UIViewDemo/ViewController.swift
+++ 07-UIKit/UIViewDemo/UIViewDemo/ViewController.swift
@@ -11,9 +11,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupStackView()
     }
 
-
+    func setupStackView() {
+        
+        // 레이블 생성
+        let label1 = UILabel()
+        label1.backgroundColor = .systemRed
+        label1.text = "Label 1"
+        
+        let label2 = UILabel()
+        label2.backgroundColor = .systemBlue
+        label2.text = "Label 2"
+        
+        let label3 = UILabel()
+        label3.backgroundColor = .systemGreen
+        label3.text = "Label 3"
+        
+//        stackView.addArrangedSubview(label1)
+//        stackView.addArrangedSubview(label2)
+//        stackView.addArrangedSubview(label3)
+        
+        let stackView = UIStackView(arrangedSubviews: [label1, label2, label3])
+        stackView.backgroundColor = .systemGray
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 }
 
