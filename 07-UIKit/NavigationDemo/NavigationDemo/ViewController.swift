79f170150cd8145bcfed2ca8d41ef53a35e5d5f3
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-18 15:45:37 +0900

    UIKit: NavigationView - 네비게이션 바 외관 설정, 스토리보드 수정: 메뉴에서  >  > 를 선택

--- 07-UIKit/NavigationDemo/NavigationDemo/ViewController.swift
+++ 07-UIKit/NavigationDemo/NavigationDemo/ViewController.swift
@@ -11,7 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "네비게이션 타이틀"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBlue
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.navigationController?.navigationBar.tintColor = .white
     }
 
 
