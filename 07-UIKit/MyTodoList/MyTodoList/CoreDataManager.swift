44fbd06493d19776a18d9378aee1a9c6d1045dde
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-21 14:39:25 +0900

    UIKit: CoreData이용한 TODOList 만들기 - 프로젝트 생성 및 gitignore 추가

---
+++ 07-UIKit/MyTodoList/MyTodoList/CoreDataManager.swift
@@ -0,0 +1,11 @@
+//
+//  CoreDataManager.swift
+//  MyTodoList
+//
+//  Created by KimJunsoo on 3/20/25.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
---
+++ 07-UIKit/MyTodoList/MyTodoList/TodoTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TodoTableViewController.swift
+//  MyTodoList
+//
+//  Created by KimJunsoo on 3/20/25.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Todo List"
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
