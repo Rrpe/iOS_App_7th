90d343c506c4ebdf0a69877239b393278c16a5e1
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-26 11:28:55 +0900

    JournalListTableViewCell에 샘플 데이터 연결 및 UI 요소 업데이트

--- 08-JRNL/JRNL/JRNL/JournalListViewController.swift
+++ 08-JRNL/JRNL/JRNL/JournalListViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 class JournalListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var sampleJournalEntryData = SampleJournalEntryData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 샘플 데이터 생성
+        sampleJournalEntryData.createSampleJournalEntryData()
     }
     
     @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
@@ -22,10 +25,22 @@ class JournalListViewController: UIViewController {
 
 extension JournalListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return sampleJournalEntryData.journalEntries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+        let journalCell = tableView.dequeueReusableCell(
+            withIdentifier: "journalCell",
+            for: indexPath
+        ) as! JournalListTableViewCell
+        
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        // 날짜, 제목, 사진 표시
+        // 날짜는 "월 일, 년" 형식으로 표시
+        journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.month().day().year())
+        journalCell.titleLabel.text = journalEntry.entryTitle
+        journalCell.photoImageView.image = journalEntry.photo
+        
+        return journalCell
     }
 }
