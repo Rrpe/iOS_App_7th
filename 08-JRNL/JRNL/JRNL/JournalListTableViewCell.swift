8f358ad2f7636a63ae1c004be06154ffdb4a6999
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-26 11:05:38 +0900

    JournalListTableViewCell 클래스 추가 및 UI 요소 연결

---
+++ 08-JRNL/JRNL/JRNL/JournalListTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  JournalListTableViewCell.swift
+//  JRNL
+//
+//  Created by KimJunsoo on 3/26/25.
+//
+
+import UIKit
+
+class JournalListTableViewCell: UITableViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+}
