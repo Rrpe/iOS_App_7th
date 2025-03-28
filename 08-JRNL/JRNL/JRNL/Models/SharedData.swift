6cb8f769109eff142d406b90cc973d1bac5476a8
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-28 11:15:59 +0900

    저널 항목 데이터 로드 및 저장 기능 구현

--- 08-JRNL/JRNL/JRNL/Models/SharedData.swift
+++ 08-JRNL/JRNL/JRNL/Models/SharedData.swift
@@ -36,10 +36,46 @@ class SharedData {
     // 데이터 추가
     func addJournalEntry(_ entry: JournalEntry) {
         journalEntries.append(entry)
+        saveJournalEntriesData()
     }
     
     // 데이터 삭제
     func removeJournalEntry(at index: Int) {
         journalEntries.remove(at: index)
+        saveJournalEntriesData()
+    }
+    
+    // MARK - Persistence
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadJournalEntriesData() {
+        let filePath = getDocumentDirectory().appendingPathComponent("journalEntriesData.json")
+        
+        do {
+            let data = try Data(contentsOf: filePath)
+            journalEntries = try JSONDecoder().decode([JournalEntry].self, from: data)
+        } catch {
+            print("Journal Entry Data를 읽어오는데 실패했습니다.")
+        }
+    }
+    
+    func saveJournalEntriesData() {
+        // 파일이 존재하지 않는 경우
+        let pathDirectory = getDocumentDirectory()
+        try? FileManager().createDirectory(at: pathDirectory, withIntermediateDirectories: true)
+        
+        
+        let filePath = getDocumentDirectory().appendingPathComponent("journalEntriesData.json")
+        
+        do {
+            let data = try JSONEncoder().encode(journalEntries)
+            /// .atomicWrite: 파일을 쓸 때 임시 파일을 만들어서 쓰고, 쓰기가 완료되면 원래 파일로 대체
+            try data.write(to: filePath, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Journal Entry Data를 저장하는데 실패했습니다.")
+        }
     }
 }
