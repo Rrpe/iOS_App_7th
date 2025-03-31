4f6f4ac8d4058498bd353eef18d7800e018a52c5
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-31 10:20:00 +0900

    UITableView에서 UICollectionView로 전환 및 관련 데이터 소스 메서드 수정 <스토리보드 수정사항> 1. 저널리스트 뷰컨트롤러의 테이블뷰 삭제 2. 컬렉션 뷰 추가 3. 데이터소스, 델리게이트 연결 4. collectionView 아웃렛 연결 5. journalCell identifier 지정

--- 08-JRNL/JRNL/JRNL/Controllers/JournalListViewController.swift
+++ 08-JRNL/JRNL/JRNL/Controllers/JournalListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class JournalListViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     let search = UISearchController(searchResultsController: nil)
     var filteredTableData: [JournalEntry] = []
@@ -33,7 +33,7 @@ class JournalListViewController: UIViewController {
         if let sourceViewController = segue.source as? AddJournalEntryViewController,
            let newJournalEntry = sourceViewController.newJournalEntry {
             SharedData.shared.addJournalEntry(newJournalEntry)
-            tableView.reloadData()
+            collectionView.reloadData()
         }
     }
     
@@ -50,62 +50,35 @@ class JournalListViewController: UIViewController {
     
 }
 
-// MARK: - UITableViewDataSource
-extension JournalListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+// MARK: - UICollectionViewDataSource
+extension JournalListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return search.isActive ? filteredTableData.count : SharedData.shared.numberOfJournalEntries
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let journalCell = tableView.dequeueReusableCell(
-            withIdentifier: "journalCell",
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let journalCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "journalCell",
             for: indexPath
-        ) as! JournalListTableViewCell
+        )
         
-        let journalEntry = search.isActive ? filteredTableData[indexPath.row] : SharedData.shared.getJounalEntry(at: indexPath.row)
+        /*let journalEntry = search.isActive ? filteredTableData[indexPath.row] : SharedData.shared.getJounalEntry(at: indexPath.row)
         // 날짜, 제목, 사진 표시
         // 날짜는 "월 일, 년" 형식으로 표시
         journalCell.dateLabel.text = journalEntry.dateString
         journalCell.titleLabel.text = journalEntry.entryTitle
         if let photoData = journalEntry.photoData {
             journalCell.photoImageView.image = UIImage(data: photoData)
-        }
+        }*/
         
         return journalCell
     }
 }
 
-// MARK: - UITableViewDelegate
-extension JournalListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("didSelectRowAt: \(indexPath)")
-        // 선택한 셀의 JournalEntry 객체를 가져와서 JournalEntryDetailViewController에 전달
-        let selectedJournalEntry = search.isActive ? filteredTableData[indexPath.row] : SharedData.shared.getJounalEntry(at: indexPath.row)
-        print("selectedJournalEntry: \(selectedJournalEntry)")
-        self.selectedJournalEntry = selectedJournalEntry
-        performSegue(withIdentifier: "showDetail", sender: self)
-    }
-    
-    func tableView(
-        _ tableView: UITableView,
-        commit editingStyle: UITableViewCell.EditingStyle,
-        forRowAt indexPath: IndexPath
-    ) {
-        if editingStyle == .delete {
-            if search.isActive {
-                let journalEntry = filteredTableData[indexPath.row]
-                SharedData.shared.removeSelected(journalEntry: journalEntry)
-                filteredTableData.remove(at: indexPath.row)
-            } else {
-                SharedData.shared.removeJournalEntry(at: indexPath.row)
-            }
-            // 테이블 전체 새로고침
-            // tableView.reloadData()
-            
-            // 테이블에서 해당 행만 삭제 ( 애니메이션 효과 포함 )
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
+// MARK: - UICollectionViewDelegate
+extension JournalListViewController: UICollectionViewDelegate {
+    // TODO: 컬렉션 뷰 델리게이트 코드 추가 ( contextMenu, 동적 사이즈 코드 추가 필요 )
 }
 
 // MARK: - UISearchResultsUpdating
@@ -119,6 +92,6 @@ extension JournalListViewController: UISearchResultsUpdating {
             return journalEntry.entryTitle.lowercased().contains(searchText.lowercased())
         }
         
-        tableView.reloadData()
+        collectionView.reloadData()
     }
 }
