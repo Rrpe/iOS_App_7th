c867a0a7cbf920f5be8845c88c69addf90af9bb6
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-02 14:37:02 +0900

    FIXME 추가: 위젯 관련

--- 08-JRNL/JRNL/JRNLWidget/JRNLWidget.swift
+++ 08-JRNL/JRNL/JRNLWidget/JRNLWidget.swift
@@ -12,23 +12,47 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), journalEntryDate: "JRNL", journalEntryTitle: "")
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), journalEntryDate: "JRNL", journalEntryTitle: "")
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         
         SharedData.shared.fetchJournalEntries()
         let journalEntries = SharedData.shared.getAllJournalEntries()
         
+        // FIXME: - 위젯에서 일기 리스트 슬라이드로 보이게 하기
+        /*if journalEntries.isEmpty {
+         let entry = SimpleEntry(
+         date: currentDate,
+         journalEntryDate: "JRNL",
+         journalEntryTitle: "작성된 일기가 없습니다."
+         )
+         entries.append(entry)
+         } else {
+         let maxEntries = min(journalEntries.count, 5)
+         
+         for minutesOffset in 0..<maxEntries {
+         let entryDate = currentDate.addingTimeInterval(TimeInterval(minutesOffset * 60))
+         let entry = SimpleEntry(
+         date: entryDate,
+         journalEntryDate: journalEntries[minutesOffset].title ?? "JRNL",
+         journalEntryTitle: journalEntries[minutesOffset].entryTitle
+         )
+         entries.append(entry)
+         }
+         }
+         let timeline = Timeline(entries: entries, policy: .atEnd)
+         completion(timeline)
+         */
         for minutesOffset in 0..<journalEntries.count {
-            let entryDate = currentDate.addingTimeInterval(TimeInterval(minutesOffset * 60))
+            let entryDate = currentDate.addingTimeInterval(TimeInterval(60 * minutesOffset))
             let entry = SimpleEntry(
                 date: entryDate,
                 journalEntryDate: journalEntries[minutesOffset].title ?? "JRNL",
@@ -36,14 +60,14 @@ struct Provider: TimelineProvider {
             )
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
+    
+    //    func relevances() async -> WidgetRelevances<Void> {
+    //        // Generate a list containing the contexts this widget is relevant in.
+    //    }
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -54,18 +78,23 @@ struct SimpleEntry: TimelineEntry {
 
 struct JRNLWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        VStack {
-            Text(entry.journalEntryDate)
-            Text(entry.journalEntryTitle)
+        ZStack {
+            AccessoryWidgetBackground()
+            VStack {
+                Text(entry.journalEntryDate)
+                    .font(.headline)
+                Text(entry.journalEntryTitle)
+                    .font(.body)
+            }
         }
     }
 }
 
 struct JRNLWidget: Widget {
     let kind: String = "JRNLWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
