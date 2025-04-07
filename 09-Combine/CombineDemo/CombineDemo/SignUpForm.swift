a2375ab8c86cd7d9b1e0d5ae142a745d2a472bf0
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-07 16:44:47 +0900

    1. 컴바인 데모 프로젝트 생성, 2. Combine 실습: 회원가입 폼 및 뷰모델 추가, 3. Combine 실습: 퍼블리셔 결합을 이용한 유효성 검사, 4. 09 네트워크 실습을 위한 서버: HTTP GET 요청을 처리하여 사용자 이름의 가용성을 확인하는 기능 추가, 5. 기존 비동기 함수를 이용한 네트워크 코드 추가: AuthenticationService 추가, 6. AuthenticationService에 Publisher를 활용한 비동기 메서드 추가 및 SignUpFormViewModel에서 사용자 이름 유효성 검사 로직 개선

---
+++ 09-Combine/CombineDemo/CombineDemo/SignUpForm.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpForm.swift
+//  CombineDemo
+//
+import SwiftUI
+
+struct SignUpForm: View {
+  @StateObject private var viewModel = SignUpFormViewModel()
+
+  var body: some View {
+    Form {
+      // 사용자 이름 입력 필드
+      Section {
+        TextField("사용자 이름", text: $viewModel.username)
+      } footer: {
+        Text(viewModel.usernameMessage)
+          .foregroundColor(.red)
+      }
+      // 비밀번호 입력 필드
+      Section {
+        SecureField("비밀번호", text: $viewModel.password)
+        SecureField("비밀번호 확인", text: $viewModel.passwordConfirmation)
+      } footer: {
+        Text(viewModel.passwordMessage)
+          .foregroundColor(.red)
+      }
+
+      // Submit 버튼
+      Section {
+        Button("Sign up") {
+          print("Sign up as \(viewModel.username)")
+        }
+        .disabled(!viewModel.isValid)
+      }
+    }
+  }
+}
+
+#Preview {
+  SignUpForm()
+}
---
+++ 09-Combine/CombineDemo/CombineDemo/SignUpFormViewModel.swift
@@ -0,0 +1,147 @@
+//
+//  SignUpFormViewModel.swift
+//  CombineDemo
+//
+
+import Foundation
+import Combine
+
+enum UserNameValid {
+    case valid
+    case tooShort
+    case notAvailable
+}
+
+typealias Available = Result<Bool, Error>
+
+extension Publisher {
+    func asResult() -> AnyPublisher<Result<Output, Failure>, Never> {
+        self
+            .map(Result.success)
+            .catch { error in Just(.failure(error)) }
+            .eraseToAnyPublisher()
+    }
+}
+
+class SignUpFormViewModel: ObservableObject {
+    // 유저 입력 프로퍼티
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    
+    // 출력 프로퍼티
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
+    
+    var authenticationService = AuthenticationService()
+    
+    // 유저이름 유효성 검사 (3자 이상)
+    private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+        $username
+            .map { $0.count >= 3 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isUsernameAvaliablePublisher: AnyPublisher<Bool, Never> = {
+        $username
+            .debounce(for: 0.8, scheduler: DispatchQueue.main)
+            .removeDuplicates()
+            .compactMap { username -> String? in
+                // 유저 이름이 비어있으면 nil을 반환
+                if username.isEmpty {
+                    return nil
+                }
+                return username
+            }
+            .flatMap { username -> AnyPublisher<Bool, Never> in
+                return self.authenticationService.checkUserNameAvailablePublisher(userName: username)
+                    .catch { error in
+                        return Just(false)
+                    }
+                    .eraseToAnyPublisher()
+                
+            }
+            .receive(on: DispatchQueue.main)
+            .share()
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isUsernameValidPublisher: AnyPublisher<UserNameValid, Never> = {
+        Publishers.CombineLatest(isUsernameLengthValidPublisher, isUsernameAvaliablePublisher)
+            .map { isLengthValid, isAvailable in
+                if !isLengthValid {
+                    return .tooShort
+                } else if !isAvailable {
+                    return .notAvailable
+                } else {
+                    return .valid
+                }
+            }
+            .eraseToAnyPublisher()
+    }()
+    
+    // 비밀번호 유효성 검사 (비밀번호가 비어있음을 검사)
+    private lazy var isPasswordEmptyPublisher: AnyPublisher<Bool, Never> = {
+        $password
+        //      .map { $0.isEmpty } 을 다음과 같이 축약해서 사용할 수 있다.
+            .map(\.isEmpty)
+            .eraseToAnyPublisher()
+    }()
+    
+    // 비밀번호와 비밀번호 확인 일치 검사
+    private lazy var isPasswordMatchingPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest($password, $passwordConfirmation)
+        //      .map { $0 == $1 } 을 다음과 같이 축약해서 사용할 수 있다.
+            .map(==)
+            .eraseToAnyPublisher()
+    }()
+    
+    // 위 두개의 비밀번호 검사 결과를 합쳐서 비밀번호 유효성 검사
+    private lazy var isPasswordValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+        // 1. isPasswordEmptyPublisher가 true 이면 비밀번호가 비어있다는 뜻이므로 isValid가 false여야 한다.
+        // 2. isPasswordMatchingPublisher가 true 이면 비밀번호와 비밀번호 확인이 일치한다는 뜻이므로 isValid가 true여야 한다.
+            .map { !$0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isFormValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isUsernameValidPublisher, isPasswordValidPublisher)
+            .map { ($0 == .valid) && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    init() {
+        // 유저 이름 유효성 검사
+        isUsernameValidPublisher
+            .map { valid in
+                switch valid {
+                case .valid:
+                    return ""
+                case .tooShort:
+                    return "사용자 이름은 3자 이상이어야 합니다."
+                case .notAvailable:
+                    return "사용자 이름이 사용 중입니다."
+                }
+            }
+            .assign(to: &$usernameMessage)
+        
+        // 비밀번호 유효성 검사
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { isEmpty, isMatching in
+                if isEmpty {
+                    return "비밀번호를 입력하세요."
+                } else if !isMatching {
+                    return "비밀번호가 일치하지 않습니다."
+                } else {
+                    return ""
+                }
+            }
+            .assign(to: &$passwordMessage)
+        
+        // 폼 유효성 검사
+        isFormValidPublisher
+            .assign(to: &$isValid)
+    }
+}
