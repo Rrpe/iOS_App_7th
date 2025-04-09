3665b2bc7c7e91261e8332252643c6f314e0e46f
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-09 14:34:17 +0900

    온디맨드 북 목록 뷰 실습: 컴바인

---
+++ 09-Combine/CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/CombineBookListView.swift
@@ -0,0 +1,43 @@
+//
+//  OnDemandBookDetailsViewWithClosures.swift
+//  CombineFirebaseBookShelf
+//
+//  Created by KimJunsoo on 4/9/25.
+//
+
+import Combine
+import SwiftUI
+import FirebaseFirestore
+
+private class BookListViewModel: ObservableObject {
+    @Published var books: [Book] = []
+    @Published var errorMessage: String?
+    
+    private var db = Firestore.firestore()
+    
+    init() {
+        db.collection("books").snapshotPublisher()
+            .tryMap { querySnapshot in
+                try querySnapshot.documents.compactMap { documentSnapshot in
+                    try documentSnapshot.data(as: Book.self)
+                }
+            }
+            .catch { error in
+                self.errorMessage = error.localizedDescription
+                return Just([Book]()).eraseToAnyPublisher()
+            }
+            .replaceError(with: [Book]())
+            .assign(to: &$books)
+    }
+}
+
+struct CombineBookListView: View {
+    @StateObject private var viewModel = BookListViewModel()
+    
+    var body: some View {
+        List(viewModel.books) { book in
+            Text(book.title)
+        }
+        .navigationTitle("Book List")
+    }
+}
