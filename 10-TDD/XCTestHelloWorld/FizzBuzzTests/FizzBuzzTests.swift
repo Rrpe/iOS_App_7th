35d92604027a011653a08de6a7c942f1796b479d
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-14 15:17:21 +0900

    FizzBuzz 테스트 케이스 추가

---
+++ 10-TDD/XCTestHelloWorld/FizzBuzzTests/FizzBuzzTests.swift
@@ -0,0 +1,35 @@
+//
+//  FizzBuzzTests.swift
+//  FizzBuzzTests
+//
+//  Created by KimJunsoo on 4/14/25.
+//
+
+import XCTest
+
+final class FizzBuzzTests: XCTestCase {
+    override func setUpWithError() throws {
+        print("setUpWithError")
+    }
+    
+    override func tearDownWithError() throws {
+        print("tearDownWithError")
+    }
+    
+    func testFizzBuzzDivisibleBy3() throws {
+        let result = fizzBuzz(3)
+        XCTAssertEqual(result, "fizz")
+    }
+    func testFizzBuzzDivisibleBy5() throws {
+        let result = fizzBuzz(5)
+        XCTAssertEqual(result, "buzz")
+    }
+    func testFizzBuzzDivisibleBy15() throws {
+        let result = fizzBuzz(15)
+        XCTAssertEqual(result, "fizz-buzz")
+    }
+    func testFizzBuzzDivisibleBy30r5ReturnUnput() throws {
+        let result = fizzBuzz(7)
+        XCTAssertEqual(result, "7")
+    }
+}
---
+++ 10-TDD/XCTestHelloWorld/XCTestHelloWorld/FizzBuzz.swift
@@ -0,0 +1,10 @@
+//
+//  FizzBuzz.swift
+//  XCTestHelloWorld
+//
+//  Created by KimJunsoo on 4/14/25.
+//
+
+public func fizzBuzz(_ number: Int) -> String {
+    return ""
+}
