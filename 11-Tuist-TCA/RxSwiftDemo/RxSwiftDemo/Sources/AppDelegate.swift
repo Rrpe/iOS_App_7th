d8cdbf821dd814cfe3861264f9a7b93f4f83d5b9
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-22 15:15:23 +0900

    RxSwift 찍먹해보기

---
+++ 11-Tuist-TCA/RxSwiftDemo/RxSwiftDemo/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  RxSwiftDemo
+//
+//  Created by KimJunsoo on 4/22/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let viewController = ParentViewController()
+    window?.rootViewController = UINavigationController(rootViewController: viewController)
+    window?.makeKeyAndVisible()
+    return true
+  }
+}
