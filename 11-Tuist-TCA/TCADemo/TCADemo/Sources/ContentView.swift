9b792b9c881867db83c83d904285e5b9ca514193
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-24 11:30:47 +0900

    TCA 다시 해보기: 코드 삭제

--- 11-Tuist-TCA/TCADemo/TCADemo/Sources/ContentView.swift
+++ 11-Tuist-TCA/TCADemo/TCADemo/Sources/ContentView.swift
@@ -2,43 +2,10 @@ import ComposableArchitecture
 import SwiftUI
 
 public struct ContentView: View {
-    let store: StoreOf<CounterFeature>
 
     public var body: some View {
         VStack {
-            Text("Count: \(store.count)")
-                .font(.largeTitle)
-                .padding()
-            
-            HStack {
-                Button("-") {
-                    store.send(.decrementButtonTapped)
-                }
-                .font(.largeTitle)
-                .padding()
-                
-                Button("+") {
-                    store.send(.incrementButtonTapped)
-                }
-                .font(.largeTitle)
-                .padding()
-            }
-            
-            if store.isLoading {
-                ProgressView()
-                    .padding()
-            }
-            
-            if let fact = store.fact {
-                Text(fact)
-                    .padding()
-                    .multilineTextAlignment(.center)
-            }
-            
-            Button("Get Number Fact") {
-                store.send(.factButtonTapped)
-            }
-            .padding()
+            Text("Hello, TCA !")
         }
         .navigationTitle("Counter")
     }
@@ -47,11 +14,5 @@ public struct ContentView: View {
 
 
 #Preview {
-    ContentView(
-        store: Store(initialState: CounterFeature.State()) {
-            CounterFeature()
-                .dependency(\.numberFact, .liveValue)
-                ._printChanges()
-        }
-    )
+    ContentView()
 }
--- 11-Tuist-TCA/TCADemo/TCADemo/Sources/TCADemoApp.swift
+++ 11-Tuist-TCA/TCADemo/TCADemo/Sources/TCADemoApp.swift
@@ -6,13 +6,7 @@ import SwiftUI
 struct TCADemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(
-                store: Store(initialState: CounterFeature.State()) {
-                    CounterFeature()
-                        .dependency(\.numberFact, .liveValue) // Preview용 의존성 주입
-                        ._printChanges()
-                }
-            )
+            ContentView()
         }
     }
 }
